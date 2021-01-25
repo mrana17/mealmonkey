d6e74add3389ec38d6f08ad01677501949e8f2fb
mrana17 <rm_a@msn.com>
2021-01-25 19:54:10 +0100

    Input Field Email created

---
+++ src/components/Input/Input.stories.js
@@ -0,0 +1,11 @@
+import "./Input.css";
+import inputMail from "./InputMail.html";
+import inputPassword from "./InputPassword.html";
+
+export default {
+  title: "Components/Input",
+  parameters: { layout: "centered" },
+};
+
+export const inputmail = () => inputMail;
+export const inputpassword = () => inputPassword;
