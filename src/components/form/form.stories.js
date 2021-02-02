09cadd605e2bdaff6507395cf6fef93dc9fece68
mrana17 <rm_a@msn.com>
2021-02-02 17:56:48 +0100

    create OTP page filled with content

--- src/components/form/form.stories.js
+++ src/components/form/form.stories.js
@@ -1,9 +1,11 @@
-import"./form.css";
+import "./form.css";
 import { createRegisterForm } from "./register-form";
+import { createVerifyForm } from "./verify-form";
 
 export default {
-    title: "Components/form",
-    parameters: {layout : "centered"},
+  title: "Components/form",
+  parameters: { layout: "centered" },
 };
 
 export const register = () => createRegisterForm();
+export const verify = () => createVerifyForm();
